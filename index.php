f0205d0ed970632aa006f97949c76737ea1671a7
Nibr92 <nibr92@mail.ru>
2018-11-11 12:37:32 +0300

    Сделал футер

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-include "header.php";
-
+    include "header.php";
+?>
+<main>
+    Это основная часть
+</main>
+<!-- Скрипты почему-то не работают -->
+<script src="jquery-3.3.1.min.js"></script>
+<script src="main1.js"></script>
+<?php
+    include ('footer.php');
 ?>
